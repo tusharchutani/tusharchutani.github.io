086d41028f71c4362c1bbbe6147b3542a385be4a
Tushar Chutani <tusharchutani@Tushars-MacBook-Pro.local>
2018-10-12 19:43:17 -0700

    Added changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,66 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Contact extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      contactEmail:"",
+      contactName:"",
+      contactMessage:"",
+      contactSubject:"",
+      buttonDisabled:true,
+      emailStatus:0 //0 nothing -1: error 1:sent 2: sending
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.sendEmail = this.sendEmail.bind(this);
+  }
+
+  handleChange(e){
+    switch (e.target.id){
+      case "contactName":
+        this.setState({contactName: e.target.value});
+      break;
+      case "contactEmail":
+        this.setState({contactEmail: e.target.value});      
+      break;
+      case "contactSubject":
+        this.setState({contactSubject: e.target.value});      
+      break;
+      case "contactMessage":
+        this.setState({contactMessage: e.target.value});      
+      break;
+      default:
+        console.log("Unknown textfield");
+    }
+    const { contactEmail, contactName, contactMessage, contactSubject } = this.state;
+    if(contactEmail && contactName && contactMessage && contactSubject){
+      this.setState({buttonDisabled:false});
+    }else{
+      this.setState({buttonDisabled:true});
+    }
+  }
+
+  async sendEmail(){
+    this.setState({buttonDisabled: true});
+    try{
+      await axios.post('https://contacttusharchutani.herokuapp.com',{
+        from: this.state.contactEmail,
+        subject: this.state.contactSubject,
+        name: this.state.contactName,
+        body: this.state.contactMessage
+      });
+      this.setState({emailStatus: 1});
+    }catch(e){
+      this.setState({emailStatus: -1});
+    }
+
+      
+     
+  }
+
+
   render() {
 
     if(this.props.data){
@@ -35,43 +95,35 @@ class Contact extends Component {
 
          <div className="row">
             <div className="eight columns">
-
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
+{this.state.emailStatus == 0    &&      <div>
+              <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
 						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>/>
+						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
                   </div>
 
                   <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>/>
+						   <label htmlFor="contactSubject">Subject <span className="required">*</span></label>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage" onChange={this.handleChange}></textarea>
                   </div>
 
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
+                  
+                     <button className="submit" disabled={this.state.buttonDisabled} onClick={this.sendEmail}>Submit</button>
 
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
+                  </div>}
+           {this.state.emailStatus == -1 && <div id="message-warning"> Error - unable to send email. Contact me on <a href="mailto:tushar_chutani@yahoo.com">tushar_chutani@yahoo.com</a> </div>}
+				   {(this.state.emailStatus == 1) && <div id="message-success">
                   <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
+				   </div>}
            </div>
 
 
@@ -86,29 +138,8 @@ class Contact extends Component {
 						   <span>{phone}</span>
 					   </p>
 				   </div>
+          </aside>
 
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
-            </aside>
       </div>
    </section>
     );
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -13,7 +13,10 @@ class Resume extends Component {
       var work = this.props.data.work.map(function (work) {
         return <div key={work.company}><h3>{work.company}</h3>
           <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-          <p>{work.description}</p>
+          <p className="description">{work.description}</p>
+          <ul>
+            {work.infoPoints.map((infoPoint)=>(<li>{infoPoint}</li>))}
+          </ul>
         </div>
       });
 
